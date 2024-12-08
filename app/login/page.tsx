60ea1d0c244825a6b772999ee9954bded53783cd
jiseob <wisdfire@gmail.com>
2024-12-08 23:27:22 +0900

    add use server, use client, useFormStatus hook, useActionState

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,28 +1,35 @@
+"use client";
 import FormButton from "@/components/form-btn";
 import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
+import { handleForm } from "./actions";
+import { useActionState } from "react";
 
 export default function Login() {
+    const [state, action] = useActionState(handleForm, null);
+
     return (
         <div className="flex flex-col gap-10 py-8 px-6">
             <div className="flex flex-col gap-2 *:font-medium">
                 <h1 className="text-2xl">안녕하세요!</h1>
                 <h2 className="text-xl">Log in with email and password.</h2>
             </div>
-            <form className="flex flex-col gap-3">
+            <form action={action} className="flex flex-col gap-3">
                 <FormInput
+                    name="email"
                     type="email"
                     placeholder="Email"
                     required
-                    errors={[""]}
+                    errors={[]}
                 />
                 <FormInput
+                    name="password"
                     type="password"
                     placeholder="Password"
                     required
-                    errors={[""]}
+                    errors={state?.errors ?? []}
                 />
-                <FormButton loading={false} text="Create account" />
+                <FormButton text="Log in" />
             </form>
             <SocialLogin />
         </div>
--- components/form-btn.tsx
+++ components/form-btn.tsx
@@ -1,15 +1,18 @@
+"use client";
+import { useFormStatus } from "react-dom";
+
 interface FormButtonProps {
-    loading: boolean;
     text: string;
 }
 
-export default function FormButton({ loading, text }: FormButtonProps) {
+export default function FormButton({ text }: FormButtonProps) {
+    const { pending } = useFormStatus();
     return (
         <button
-            disabled={loading}
-            className="primary-btn h-10 disabled:bg-neutral-400 disabled:text-neutral-300 disabled:text-neutral-300 disabled:cursor-not-allowed"
+            disabled={pending}
+            className="primary-btn h-10 disabled:bg-neutral-400 disabled:text-neutral-300 disabled:cursor-not-allowed"
         >
-            {loading ? "Loading..." : text}
+            {pending ? "Loading..." : text}
         </button>
     );
 }
